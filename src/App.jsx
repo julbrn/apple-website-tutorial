a14b085bab4e90ecfba399beca9f14d87d57778e
julbrn <juliaburenko@yandex.ru>
2025-11-10 15:50:44 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main className="text-3xl underline">
+      <Navbar />
+    </main>
+  );
+};
+
+export default App;
