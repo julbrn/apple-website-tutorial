82a567d49338274630e9e7443e6d1538024a6062
julbrn <juliaburenko@yandex.ru>
2025-11-18 14:54:41 +0100

    add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main className="text-3xl underline">
+    <main>
       <Navbar />
+      <Hero />
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img src="./logo.svg" alt="Apple logo" />
+        <ul>
+          {navLinks.map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="./search.svg" alt="search" />
+          </button>
+          <button>
+            <img src="./cart.svg" alt="cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
